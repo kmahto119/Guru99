4ecf8ccda9ac39c816e325f1863db69ee0645710
kailash <kailash007ranchi@gmail.com>
2020-01-07 09:54:29 +0530

    first commit

---
+++ src/test/java/firstDemo/NewTest.java
@@ -0,0 +1,27 @@
+package firstDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	private WebDriver driver;		
+	@Test				
+	public void testEasy() {	
+		driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+	}	
+	@BeforeTest
+	public void beforeTest() {	
+	    driver = new ChromeDriver(); 
+	}		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}	
+
+}
